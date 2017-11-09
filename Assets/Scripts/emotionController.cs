d99f8e7dbf9035f5103615b42e7530ccab3d5d23
Unknown <ricardo.filipe.silva@hotmail.com>
2017-11-09 19:42:39 +0000

    Asset\Layout\UI cleaning

--- Assets/Scripts/emotionController.cs
+++ Assets/Scripts/emotionController.cs
@@ -5,26 +5,26 @@ using UnityEngine.UI;
 
 public class emotionController : MonoBehaviour {
 
-    public Text emotionText;
-    public GameObject avatar;
-    private string emotion;
-    private Animator anim;
+	public Text emotionText;
+	public GameObject avatar;
+	private string emotion;
+	private Animator anim;
 
-    void Start ()
-    {
-        emotion = "No Emotion";
-        anim = avatar.GetComponent<Animator>();
-        //anim.Stop();
-    }
+	void Start ()
+	{
+		emotion = "No Emotion";
+		anim = avatar.GetComponent<Animator>();
+		//anim.Stop();
+	}
 	
 	void Update ()
-    {
-        emotionText.text = emotion;
-    }
+	{
+		emotionText.text = emotion;
+	}
 
-    public void playAnimation(string s)
-    {
-        emotion = s;
-        anim.Play(s, -1, 0f);
-    }
+	public void playAnimation(string s)
+	{
+		emotion = s;
+		anim.Play(s, -1, 0f);
+	}
 }
